3a932e853c1bb17f7a1b6aec3efddca27a4ac942
Vitalii <saifutdinov_vitalii@mail.ru>
2025-08-14 15:13:05 +0500

    three pages in developer

---
+++ assets/js/slider.js
@@ -0,0 +1,18 @@
+const  product = new Swiper(".swiper-product", {
+    loop: true,
+    spaceBetween: 10,
+    slidesPerView: 4,
+    freeMode: true,
+    watchSlidesProgress: true,
+});
+const swiper2 = new Swiper(".mySwiper2", {
+    loop: true,
+    spaceBetween: 10,
+    navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+    },
+    thumbs: {
+    swiper: product,
+    },
+});
