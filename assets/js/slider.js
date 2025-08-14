668876c40b25ef6c481f1bf8336a7e8549bf8dcb
Vitalii <saifutdinov_vitalii@mail.ru>
2025-08-14 16:24:18 +0500

    add slider

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,18 +1,18 @@
-const  product = new Swiper(".swiper-product", {
-    loop: true,
-    spaceBetween: 10,
-    slidesPerView: 4,
-    freeMode: true,
-    watchSlidesProgress: true,
-});
-const swiper2 = new Swiper(".mySwiper2", {
-    loop: true,
-    spaceBetween: 10,
-    navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-    },
-    thumbs: {
-    swiper: product,
-    },
-});
+var swiper = new Swiper(".mySwiper", {
+      loop: true,
+      spaceBetween: 10,
+      slidesPerView: 4,
+      freeMode: true,
+      watchSlidesProgress: true,
+    });
+    var swiper2 = new Swiper(".mySwiper2", {
+      loop: true,
+      spaceBetween: 10,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      thumbs: {
+        swiper: swiper,
+      },
+    });
